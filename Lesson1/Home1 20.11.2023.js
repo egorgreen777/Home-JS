8b4c434d4dc950c1abdc8be0aa9b4da1dbd3deec
egorgreen777 <egor.green777@gmail.com>
2023-11-21 18:13:58 +0200

    Home Work 20.11

---
+++ Lesson1/Home1 20.11.2023.js
@@ -0,0 +1,60 @@
+// let Home120112023 = 123;
+let home120112023;
+home120112023 = 123;
+let home7 = 1;
+home7 = "1";
+let home8 = 10;
+home8 = 10;
+let home9 = -999;
+home9 = -999;
+let home10 = 3.14;
+home10 = 3.14;
+let home11 = 2.7;
+home11 = 2.7;
+let home12 = 16;
+home12 = 16;
+
+
+
+let home = "hello";
+home = "hello";
+let home4 = "owu";
+home4 = "owu";
+let home5 = "com";
+home5 = "com";
+let home6 = "ua";
+home6 = "ua";
+
+
+
+let home2 = false;
+let home3 = true;
+
+console.log(home120112023);
+console.log(home7);
+console.log(home8);
+console.log(home9);
+console.log(home10);
+console.log(home11);
+console.log(home12);
+
+
+console.log(home);
+console.log(home4);
+console.log(home5);
+console.log(home6);
+
+
+console.log(home2);
+console.log(home3);
+
+
+
+
+
+let a = 100;
+console.log(typeof a);
+let b = '100';
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
